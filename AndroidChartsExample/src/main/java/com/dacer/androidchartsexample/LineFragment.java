24eb16c2d6ba00b96fea637260dbd5e206c63d88
dacer <qqwerrd@gmail.com>
2014-01-21 17:41:03 +0800

    minor change

--- AndroidChartsExample/src/main/java/com/dacer/androidchartsexample/LineFragment.java
+++ AndroidChartsExample/src/main/java/com/dacer/androidchartsexample/LineFragment.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
  * Created by Dacer on 11/15/13.
  */
 public class LineFragment extends Fragment {
-    int randomint = (int)(Math.random()*30+6);
+    int randomint = 9;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,7 +30,7 @@ public class LineFragment extends Fragment {
         }
         lineView.setBottomTextList(test);
         lineView.setDrawDotLine(true);
-        lineView.setShowPopup(LineView.SHOW_POPUPS_MAXMIN_ONLY);
+        lineView.setShowPopup(LineView.SHOW_POPUPS_NONE);
 
         Button lineButton = (Button)rootView.findViewById(R.id.line_button);
         lineButton.setOnClickListener(new View.OnClickListener() {
@@ -47,19 +47,19 @@ public class LineFragment extends Fragment {
 
     private void randomSet(LineView lineView){
         ArrayList<Integer> dataList = new ArrayList<Integer>();
-        int random = (int)(Math.random()*99+1);
+        int random = (int)(Math.random()*9+1);
         for (int i=0; i<randomint; i++){
             dataList.add((int)(Math.random()*random));
         }
         
         ArrayList<Integer> dataList2 = new ArrayList<Integer>();
-        random = (int)(Math.random()*99+1);
+        random = (int)(Math.random()*9+1);
         for (int i=0; i<randomint; i++){
 			dataList2.add((int)(Math.random()*random));
         }
 
         ArrayList<Integer> dataList3 = new ArrayList<Integer>();
-        random = (int)(Math.random()*99+1);
+        random = (int)(Math.random()*9+1);
         for (int i=0; i<randomint; i++){
             dataList3.add((int)(Math.random()*random));
         }
@@ -67,7 +67,7 @@ public class LineFragment extends Fragment {
         ArrayList<ArrayList<Integer>> dataLists = new ArrayList<ArrayList<Integer>>();
         dataLists.add(dataList);
         dataLists.add(dataList2);
-        dataLists.add(dataList3);
+//        dataLists.add(dataList3);
         
         lineView.setDataList(dataLists);
     }
